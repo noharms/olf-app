89f0b01bc14752e4d6482f3a45e64dcaf324e6b0
noharms <harms.no@gmail.com>
2023-05-26 18:10:44 +0200

    Add new home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CurrentGameComponent } from './current-game/current-game.component';
 import { CardComponent } from './current-game/card/card.component';
 import { GameOverModalComponent } from './current-game/game-over-modal/game-over-modal.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrentGameComponent,
     CardComponent,
-    GameOverModalComponent
+    GameOverModalComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
