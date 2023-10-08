0b77c0ce91943c9c63521b94699cb6df1ecd23d9
noharms <harms.no@gmail.com>
2023-10-08 15:46:37 +0200

    Fix order for routing options (wildcard at end)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { CurrentGameComponent } from './current-game/current-game.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '**', component: HomeComponent },
   { path: 'olf/home', component: HomeComponent },
   { path: 'olf/cardgame', component: CurrentGameComponent },
+  { path: '**', component: HomeComponent } // caveat: this needs to be at the end, because order matters in finding a matching route here
 ];
 
 @NgModule({
