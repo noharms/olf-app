37cf5e5dd2b705df724015a37637b362de570faf
noharms <harms.no@gmail.com>
2024-04-01 13:23:23 +0200

    Add authentication service

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { MOCK_USERS } from 'src/mocks/mock-user-data';
+import { User } from 'src/model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User | null>;
+
+  private static readonly CURRENT_USER_KEY = 'currentUser';
+
+  constructor() {
+    // Initialize the currentUserSubject with the user from local storage if available
+    const storedUser: User = this.getCurrentUserFromLocalStorage();
+    this.currentUserSubject = new BehaviorSubject<User | null>(storedUser);
+  }
+
+  private getCurrentUserFromLocalStorage(): User {
+    const currentUserJson: string = localStorage.getItem(AuthenticationService.CURRENT_USER_KEY) ?? 'null';
+    return JSON.parse(currentUserJson);
+  }
+
+  public get currentUserValue(): User | null {
+    return this.currentUserSubject.value;
+  }
+
+  public get currentUser(): Observable<User | null> {
+    return this.currentUserSubject.asObservable();
+  }
+
+  login(username: string, password: string): void {
+    // TODO
+    // Placeholder for login logic, should be replaced with real authentication logic
+    // For demonstration, we assume login is successful and create a dummy user
+    const user: User = MOCK_USERS[1];
+
+    // In a real scenario, you'd get the user object from your backend
+    // and possibly a token which you'd want to store as well
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+
+  logout(): void {
+    // Remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
